cf2357e62c8fe4aad599e9652833f09325ab048f
Fletcher Walmsley <67030151+fletcherpwalmsley@users.noreply.github.com>
2024-09-05 22:15:29 +0000

    Update deploy dockerfile with gstreamer depends Change main to allow video name to be passed in Vscode wanted to change file.associations list again ¯\(°_o)/¯

--- deploy/src/main.cpp
+++ deploy/src/main.cpp
@@ -1,23 +1,21 @@
 #include <cstdio>
 #include <iostream>
 #include <memory>
-#include <utility>
-
+// #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <utility>
+// #include <opencv2/highgui/highgui.hpp>
+// #include <opencv2/imgcodecs.hpp>
 
 #include "cnn_runner.h"
 #include "river_mask_generator.h"
-
 
 int main(int argc, char* argv[]) {
   // if (argc != 2) {
   //   fprintf(stderr, "minimal <tflite model>\n");
   //   return 1;
   // }
-  // const char* image_filename = argv[1];
+  const char* video_filename = argv[1];
 
   // std::string image_filename = "image.png";
   // cv::Mat import_image = cv::imread(image_filename, cv::IMREAD_COLOR);
@@ -26,10 +24,10 @@ int main(int argc, char* argv[]) {
   //   return -1;
   // }
 
-  std::cout << cv::getBuildInformation(); 
-  cv::VideoCapture cap("/workspaces/Navigator/deploy/build/video.mp4");
-  if(!cap.isOpened()){
-    std::cout << "Error opening video stream or file" << std::endl;
+  std::cout << cv::getBuildInformation();
+  cv::VideoCapture cap(video_filename);
+  if (!cap.isOpened()) {
+    std::cout << "Error opening video file: " << video_filename << std::endl;
     return -1;
   }
 
@@ -37,23 +35,24 @@ int main(int argc, char* argv[]) {
   int frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
   int frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
   int number_of_frames = cap.get(cv::CAP_PROP_FRAME_COUNT);
-   
+
   // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
-  cv::VideoWriter video("mask_video.avi", cv::VideoWriter::fourcc('M','J','P','G'), 30, cv::Size(frame_width,frame_height));
-  
-  // Setup mask network 
+  cv::VideoWriter video("mask_video.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30,
+                        cv::Size(frame_width, frame_height));
+
+  // Setup mask network
   std::shared_ptr<CNNRunner> runner = std::make_unique<TFliteRunner>("model.tflite");
   RiverMaskGenerator river_mask_generator(runner);
-  
-  int frame_num = 0; 
-  while(1) {
+
+  int frame_num = 0;
+  while (1) {
     cv::Mat frame;
     cv::Mat mask;
     cv::Mat colour_correct_image;
     cap >> frame;
-    if(frame.empty()) {
+    if (frame.empty()) {
       break;
-    }     
+    }
     cv::cvtColor(frame, colour_correct_image, cv::COLOR_BGR2RGB);
     mask = river_mask_generator.GenerateMask(colour_correct_image);
     video.write(mask);
@@ -63,7 +62,7 @@ int main(int argc, char* argv[]) {
   // When everything done, release the video capture and write object
   cap.release();
   video.release();
-  
+
   // Save the mask
   // cv::imwrite("mask.jpeg", mask);
   return 0;
