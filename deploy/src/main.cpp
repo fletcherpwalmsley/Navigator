041f82f951063cf115f5c78cf88652cdda3742cb
Fletcher Walmsley <fletcherpw99@gmail.com>
2024-12-19 21:45:39 +0000

    Output the masked overlaid over the original image

--- deploy/src/main.cpp
+++ deploy/src/main.cpp
@@ -11,10 +11,10 @@
 #include "river_mask_generator.h"
 
 int main(int argc, char* argv[]) {
-  // if (argc != 2) {
-  //   fprintf(stderr, "minimal <tflite model>\n");
-  //   return 1;
-  // }
+  if (argc != 2) {
+    fprintf(stderr, "navigator video_path\n");
+    return 1;
+  }
   const char* video_filename = argv[1];
 
   // std::string image_filename = "image.png";
@@ -35,30 +35,40 @@ int main(int argc, char* argv[]) {
   int frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
   int frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
   int number_of_frames = cap.get(cv::CAP_PROP_FRAME_COUNT);
+  int fps = static_cast<int>(cap.get(cv::CAP_PROP_FPS)) / 10;
 
   // Setup mask network
-  std::shared_ptr<CNNRunner> runner = std::make_unique<TFliteRunner>("model.tflite");
+  std::shared_ptr<CNNRunner> runner = std::make_unique<TFliteRunner>("../../../training/output/model.tflite");
   RiverMaskGenerator river_mask_generator(runner);
 
-  // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
-  // cv::VideoWriter video("mask_video.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30,
-  //                       cv::Size(frame_width, frame_height));
   cv::VideoWriter video("mask_video.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30,
                         cv::Size(runner->GetOutputWidth(), runner->GetOutputHeight()), false);
 
   int frame_num = 0;
+  int next_frame_num = frame_num + fps;
   while (1) {
     cv::Mat frame;
     cv::Mat mask;
     cv::Mat colour_correct_image;
-    cap >> frame;
+    while (frame_num++ < next_frame_num) {
+      cap >> frame;
+    }
+    next_frame_num = frame_num + fps;
     if (frame.empty()) {
       break;
     }
     cv::cvtColor(frame, colour_correct_image, cv::COLOR_BGR2RGB);
     mask = river_mask_generator.GenerateMask(colour_correct_image);
-    video.write(mask);
-    std::cout << "Processed frame number " << frame_num++ << " of " << number_of_frames << std::endl;
+
+    cv::Mat resizedFrame;
+    cv::Mat grayScaleFrame;
+    cv::Mat outFrame;
+    cv::resize(frame, resizedFrame, mask.size(), cv::INTER_NEAREST);
+    cv::cvtColor(resizedFrame, grayScaleFrame, cv::COLOR_BGR2GRAY);
+    cv::addWeighted(grayScaleFrame, 0.6, mask, 0.4, 0, outFrame);
+
+    video.write(outFrame);
+    std::cout << "Processed frame number " << frame_num << " of " << number_of_frames << std::endl;
   }
 
   // When everything done, release the video capture and write object
